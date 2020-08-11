da44ff0c9b03fce9b1d9f8907969459413dfc065
AlecTech <eremeevoleg@gmail.com>
2020-08-11 11:15:35 -0600

    calculator example

--- Program.cs
+++ Program.cs
@@ -6,17 +6,29 @@ namespace Assignement2
     {
         static void Main(string[] args)
         {
+            int result;
+            int leftOperand, rightOperand;
+            Console.WriteLine("Please enter the left operand");
+            leftOperand = int.Parse(Console.ReadLine());
+
+             Console.WriteLine("Please enter the right operand");
+            rightOperand = int.Parse(Console.ReadLine());
+
+            result = leftOperand * rightOperand;
+
+            Console.WriteLine("total" + result);
             
-           int numYear = DateTime.Now.Year;
+
+           //int numYear = DateTime.Now.Year;
             //int year = Int32.Parse(numYear);
 
-            Console.WriteLine("Type your Age");
-            string age = Console.ReadLine();
-            int numAge = Int32.Parse(age);
+            //Console.WriteLine("Type your Age");
+           // string age = Console.ReadLine();
+           // int numAge = Int32.Parse(age);
            
-           int birthYear = numYear - numAge;
+          // int birthYear = numYear - numAge;
 
-            Console.WriteLine("Your BirthDay is " + birthYear);
+           // Console.WriteLine("Your BirthDay is " + birthYear);
 
            // Console.WriteLine("Type your name in lower case");
             //string name = Console.ReadLine();
