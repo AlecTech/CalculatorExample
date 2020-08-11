d2ffb5bcabfba8069a3a4fcaf4c0d90c24ee8b11
AlecTech <eremeevoleg@gmail.com>
2020-08-11 11:07:28 -0600

    second commit

--- Program.cs
+++ Program.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 
-namespace Aug11
+namespace Assignement2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.ReadLine();
             
-            Console.WriteLine("Hello World!");
+           int numYear = DateTime.Now.Year;
+            //int year = Int32.Parse(numYear);
+
+            Console.WriteLine("Type your Age");
+            string age = Console.ReadLine();
+            int numAge = Int32.Parse(age);
+           
+           int birthYear = numYear - numAge;
+
+            Console.WriteLine("Your BirthDay is " + birthYear);
+
+           // Console.WriteLine("Type your name in lower case");
+            //string name = Console.ReadLine();
+            //string upperCase = name.ToUpper();
+           // Console.WriteLine(name);
+           // Console.WriteLine(upperCase);
         }
     }
 }
